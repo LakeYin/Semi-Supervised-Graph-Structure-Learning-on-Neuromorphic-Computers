f739b1d8dddd24c1f00c0ca250fc2df92fb8ec7b
Lake Yin <la11010e@gmail.com>
2024-04-12 17:45:02 -0400

    Finally got it working

--- utils.py
+++ utils.py
@@ -7,9 +7,9 @@ def setup_nest(G, training_set):
     classes = set(class_table.values())
 
     # We turn off leak for both sets of neurons by setting their τm (membrane time constants) value to 100,000 ms, and we set their reset voltages to 0 mV and their threshold voltages to 1 mV
-    #citation_neurons = nest.Create("iaf_psc_delta", len(G), {"tau_m": 100000, "V_reset": 0, "V_th": 1, "tau_minus": 30})
-    citation_neurons = nest.Create("iaf_psc_alpha", len(G), {"I_e": 376})
-    class_neurons = nest.Create("iaf_psc_alpha", len(classes), {"tau_m": 100000, "V_reset": 0, "V_th": 1, "tau_minus": 30})
+    # NOTE: I_e not documented in paper, but most examples seem to use 376 as a default value, and it doesn't work without this
+    citation_neurons = nest.Create("iaf_psc_delta", len(G), {"tau_m": 100000, "V_reset": 0, "V_th": 1, "I_e": 376})
+    class_neurons = nest.Create("iaf_psc_delta", len(classes), {"tau_m": 100000, "V_reset": 0, "V_th": 1, "I_e": 376, "tau_minus": 30})
 
     citation_neurons = {label: neuron for label, neuron in zip(G.nodes(), citation_neurons)}
     class_neurons = {c: neuron for c, neuron in zip(range(len(classes)), class_neurons)}
@@ -23,9 +23,9 @@ def setup_nest(G, training_set):
     training_set = set(training_set)
 
     # For the test neurons, we set their refractory periods to be very high
-    #for n in G.nodes():
-    #    if n not in training_set:
-    #        citation_neurons[n].set({"t_ref": 1})
+    for n in G.nodes():
+        if n not in training_set:
+            citation_neurons[n].set({"t_ref": 100000})
 
     # For each edge connecting nodes A and B in the citation network, we create two synapses: one from A to B and one from B to A. We set these synapses to have a weight of 100 and a delay of 1 ms.
     for u, v in G.edges():
@@ -36,7 +36,7 @@ def setup_nest(G, training_set):
         # Since we know the topics of the papers in the training set, we create two synapses between paper N’s neuron and topic T’s neuron: one from N to T and one from T to N, each with a weight of 1 and delay of 1.
         if n in training_set:
             nest.Connect(citation_neurons[n], class_neurons[c], syn_spec={"weight": 1, "delay": 1})
-            nest.Connect(class_neurons[c], citation_neurons[n], syn_spec={"weight": 1, "delay": 1})
+            nest.Connect(class_neurons[c], citation_neurons[n], syn_spec={ "weight": 1, "delay": 1})
         # we create a synapse to and from each of the topics for each paper neuron in the testing set. Those synapses have a weight of 0.0001 and a delay of 1 ms. The synapses between papers and topics all have STDP turned on, with a τ− (time constant for the depression part of STDP window) value of 30 ms.
         else:
             for c_t in classes:
@@ -49,7 +49,7 @@ def run_simulation(test_node_id, citation_neurons, class_neurons, time_steps=20,
     test_neuron = citation_neurons[test_node_id]
 
     # we create a single spike on the corresponding testing neuron at simulation time 1
-    spike = nest.Create("spike_generator", params={"spike_times": [1.0], "spike_weights": [1e4]})
+    spike = nest.Create("spike_generator", params={"spike_times": [1.0], "spike_weights": [1e3]})
     nest.Connect(spike, test_neuron)
 
     # default nest value
@@ -61,11 +61,11 @@ def run_simulation(test_node_id, citation_neurons, class_neurons, time_steps=20,
     # Then, we look at the synapses between all of the topics and that test paper (from the topic to the test paper)
     # It is worth noting that we could similarly check the highest weighted synapse between the test paper and all of the topics.
     result_table = {}
-    for c, neuron in class_neurons.items():
-        weight = nest.GetConnections(source=test_neuron, target=neuron).get(["weight"])
+    for c, class_neuron in class_neurons.items():
+        weight = nest.GetConnections(source=class_neuron, target=test_neuron).get(["weight"])
         result_table[c] = weight["weight"]
 
-    highest = max(result_table.items(), key=lambda x: x[1])[0]
+    highest = min(result_table.items(), key=lambda x: x[1])[0]
 
     if reset:
         nest.ResetKernel()
